9fc14f951c162689e2850c1d03fa9f3fc942bfd4
luffybhai145-gif <luffybhai145@gmail.com>
2026-08-14 12:41:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,17 +17,17 @@ def required(name: str) -> str:
     return value
 
 
-API_ID = int(required("API_ID"))
+API_ID = int(required("15055049"))
 
-API_HASH = required("API_HASH")
+API_HASH = required("abe3f66fcd80c91e53009ba52c7b3a83")
 
-BOT_TOKEN = required("BOT_TOKEN")
+BOT_TOKEN = required("8784253318:AAGkOy2f650Jzlr-x8XvcuaMVQmd-BBHumA")
 
-DATABASE_URL = required("DB_URL")
+DATABASE_URL = required("mongodb+srv://newsudo:786780@cluster0.pbiae8a.mongodb.net/?appName=Cluster0")
 
 ADMIN_IDS = {
     int(user_id.strip())
-    for user_id in required("ADMIN_IDS").split(",")
+    for user_id in required("ADMIN_IDS").split("7653921320")
     if user_id.strip()
 }
 
